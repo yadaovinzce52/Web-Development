b193956ba5cd7b7774d265576dd70f2c434159cf
Vinzce Yadao <yadaovinzce@gmail.com>
2024-11-18 20:33:18 -0500

    Commpleted html parser

--- Top 100 Movies/main.py
+++ Top 100 Movies/main.py
@@ -5,4 +5,13 @@ URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/m
 
 # Write your code below this line 👇
 
+response = requests.get(URL)
+top_movies = response.text
+
+soup = BeautifulSoup(top_movies, "html.parser")
+titles = soup.find_all("h3")
+
+with open("movies.txt", "w", encoding="utf-8") as file:
+    for title in titles[::-1]:
+        file.writelines(f"{title.text}\n")
 
